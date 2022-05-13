adef623921731fdf7a455f08e8ec325ab1a42eec
Yu Asabe <yuasabe@g.ecc.u-tokyo.ac.jp>
2022-05-13 17:53:26 +0900

    Implement split function in create_splits.py

--- create_splits.py
+++ create_splits.py
@@ -6,6 +6,8 @@ import random
 import numpy as np
 
 from utils import get_module_logger
+from sklearn.model_selection import train_test_split
+import shutil
 
 
 def split(source, destination):
@@ -17,7 +19,22 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+    tfrecords = glob.glob(os.path.join(source, "*.tfrecord"))
+    train_files, val_test_files = train_test_split(tfrecords, test_size=0.4)
+    val_files, test_files = train_test_split(val_test_files, test_size=0.5)
+    
+    train_dir = os.path.join(destination, 'train')
+    val_dir = os.path.join(destination, 'val')
+    test_dir = os.path.join(destination, 'test')
+    
+    dirs = [train_dir, val_dir, test_dir]
+    files = [train_files, val_files, test_files]
+    
+    for d, fs in zip(dirs, files):
+        print(f"Moving files to {d}.")
+        for file in fs:
+            shutil.move(file, d)
+        print(f"Loaded {len(fs)} files.")
 
 
 if __name__ == "__main__":
